e872a7ea28ca30f4fcb2691f23d5bb80da1c48e8
Dav <dav.trepsiuq>
2024-06-13 12:07:49 -0400

    public:publicando la pagina

---
+++ js/about.e0905e8d.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunktiendaropamujer"]=self["webpackChunktiendaropamujer"]||[]).push([[594],{644:function(u,e,n){n.r(e),n.d(e,{default:function(){return c}});var t=function(){var u=this,e=u._self._c;return e("div",{staticClass:"about"})},a=[],r={name:"AboutView"},l=r,s=n(656),i=(0,s.A)(l,t,a,!1,null,null,null),c=i.exports}}]);
+//# sourceMappingURL=about.e0905e8d.js.map
